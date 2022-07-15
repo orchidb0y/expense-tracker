e6bef9d956d99c76556e5c3c2c2e080925a0e5fa
David Sobral <davidsobral@me.com>
2022-07-15 20:23:00 -0300

    Added checks for create_user and create_account, added isfloat() helper function

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from multiprocessing.sharedctypes import Value
 import sqlite3 as sql
 from datetime import date
 from os.path import exists
+
+
+def isfloat(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
 
 
 # This class will handle input and output to and from the expense database
@@ -23,30 +32,34 @@ class db_manager:
             if table[0] == 0:
                 self.cur.execute('''CREATE TABLE expenses
                                     (id INTEGER PRIMARY KEY,
-                                    establishment VARCHAR(50),
-                                    account VARCHAR(20),
+                                    establishment VARCHAR(20),
+                                    account VARCHAR(10),
                                     year INTEGER,
                                     month INTEGER,
                                     day INTEGER)''')
-        
         # Check if accounts table exists, creates it if not
         for table in self.cur.execute("SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='accounts'"):
             print(table) # debug print
             if table[0] == 0:
                 self.cur.execute('''CREATE TABLE accounts
-                                    (account VARCHAR(20) PRIMARY KEY,
-                                    user VARCHAR(20),
+                                    (account VARCHAR(10) PRIMARY KEY,
+                                    user VARCHAR(10),
                                     balance MONEY)''')
-        
         # Check if users table exists, creates it if not
         for table in self.cur.execute("SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='users'"):
             print(table) # debug print
             if table[0] == 0:
                 self.cur.execute('''CREATE TABLE users
-                                    (user VARCHAR(20) PRIMARY KEY,
-                                    email VARCHAR(30))''')
+                                    (user VARCHAR(10) PRIMARY KEY,
+                                    email VARCHAR(20))''')
 
     def create_user(self, user, email):
+        if user.isnumeric() or email.isnumeric():
+            return ValueError('User name/Email cannot be numeric')
+        elif ('@' not in email):
+            return ValueError('Entered invalid email')
+        elif len(user) > 10 or len(email) > 20:
+            return ValueError('User/email entered is too long (max of 10 char for user and 30 char for email')
         self.cur.execute('''INSERT INTO users
                             VALUES (?,
                             ?)''', (user, email))
@@ -55,8 +68,13 @@ class db_manager:
     def create_account(self, account_name, user, initial_balance):
         # Check if user exists
         if not bool(self.cur.execute("SELECT COUNT(*) FROM users WHERE user=:user", {'user': user}).fetchone()[0]):
-            return NameError('This user does not exist in the database.')
-        # In the future, add checks to see if the arguments account_name, user and initial balance are appropriate
+            return NameError('Entered user does not exist in the database')
+        elif account_name.isnumeric():
+            return ValueError('Account name cannot be numeric')
+        elif len(account_name) > 10:
+            return ValueError('Entered account name is too long')
+        elif not isfloat(initial_balance):
+            return ValueError('Entered initial balance is not valid (only accepts integers or floats)')
         else:
             self.cur.execute('''INSERT INTO accounts
                                 VALUES (?,
@@ -67,8 +85,8 @@ class db_manager:
 
 # Testing ground (while I don't get a handle on pytest)
 manager = db_manager()
-manager.create_user('David', 'myemail@email.com')
-print(manager.create_account('Mybank', 'David', 50))
+print(manager.create_user('David', 'myemail@email.com'))
+print(manager.create_account('Bank', 'David', 50))
 
 for row in manager.cur.execute('SELECT * FROM users'):
     print(row)
